2973b92823b7af50500c5ada839dafa9e0ede29d
Tom <tom.palleau@gmail.com>
2023-06-01 17:30:47 +0200

    initial empty activity

---
+++ app/src/main/java/com/example/cooklette/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cooklette
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
